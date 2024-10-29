e3bdae2cc4043927c833478878459167d52607c6
Mohammad Sheakh <mohammad.sheakh@gmail.com>
2024-10-29 14:07:23 +0600

    Test Msg 2
    |-> New tips
    
    |-> Try Again
    
    |->Hello from new line
    
    |-> Last line

--- LeetCode-75/11-E-Is_Subsequence-392.js
+++ LeetCode-75/11-E-Is_Subsequence-392.js
@@ -15,5 +15,5 @@ var Is_Subsequence = function (s, t) {
   let i = 0; // Pointer for string s
   let j = 0; // Pointer for string t
 };
-console.log("Move_Zeroes([0,1,0,3,12]) :: ", Is_Subsequence("Mo", "omu")); // Output: true or false ..
+console.log("Move_Zeroes([0,1,0,3,12]) :: ", Is_Subsequence("Mo", "omue")); // Output: true or false ..
 // Move_Zeroes([0,1,0,3,12]) ::  [ 1, 3, 12, 0, 0 ]
