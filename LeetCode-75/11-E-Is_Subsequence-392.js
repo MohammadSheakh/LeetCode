9d45a89c685572046053f47044361a4e7540b2d3
Mohammad Sheakh <mohammad.sheakh@gmail.com>
2025-03-09 13:01:54 +0600

    test

--- LeetCode-75/11-E-Is_Subsequence-392.js
+++ LeetCode-75/11-E-Is_Subsequence-392.js
@@ -17,3 +17,24 @@ var Is_Subsequence = function (s, t) {
 };
 console.log("Move_Zeroes([0,1,0,3,12]) :: ", Is_Subsequence("Mo", "omue")); // Output: true or false ..
 // Move_Zeroes([0,1,0,3,12]) ::  [ 1, 3, 12, 0, 0 ]
+
+/*
+ <!-- Solution From Online -->
+
+ bool isSubsequence(string s, string t) {
+        int i=0,j=0;
+        int n = s.size();
+        int m = t.size();
+        while(i<n && j<m){
+            if(s[i] == t[j]){
+                i++,j++;
+            }else{
+                j++;
+            }
+        }
+
+        if(i==n) return true;
+        return false;
+
+    }
+*/
