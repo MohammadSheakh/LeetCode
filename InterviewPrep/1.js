ee81c5f0d209dfa22fe491c7d921be30142035eb
Mohammad Sheakh <mohammad.sheakh@gmail.com>
2025-06-17 23:29:47 +0600

    1.js

--- InterviewPrep/1.js
+++ InterviewPrep/1.js
@@ -11,6 +11,669 @@
  */
 
 /**
+  * @NEW_TOPIC <!-- Runtime Environment -->
+    provides an environment to run code
+    includes services like file system, network, memory management,IO operations, etc.
+    
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!-- What are the 7 main feature of node js  -->
+    1. Asynchronous 
+    2. Event Driven
+    3. Single Threaded
+    4. Non-blocking I/O
+    5. Cross Platform
+    6. Fast Execution
+    7. Scalable
+    8. Open Source
+    9. Rich Ecosystem npm (Node Package Manager)
+    10. Real time capabilities .. bi directional communication
+    11. V8 Engine
+
+    @Next <!--    -->
+    
+ */
+
+
+/**
+  * @NEW_TOPIC <!-- Synchronous -->
+    Each Task is executed one after another
+    If one task takes a long time to complete, it will block the execution of other tasks
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!-- Multiple thread can create deadlock problem -->
+
+    Asynchronous flow can be achieved bu its single threaded, non-blocking and event driven
+    architecture 
+
+
+    1. in synchronous programming, tasks are executed one after another in a sequential manner
+    1. in asynchronous programming, tasks can be executed independently and concurrently
+
+    2. 
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!-- Event, Event Emitter, Event Queue, Event Loop, Event Driven -->
+
+    Event : Signal that something has happened in a program
+
+    Event Emitter : Create or emit event 
+
+    Event Queue : Emitted events are placed in an event queue, waiting to be processed
+
+    Event Handler : Function that listens for and responds to events
+
+    Event Loop : The event loop picks up event from the event queue and executes them in
+            the order they were received, allowing for non-blocking execution
+
+    Event Driven Architecture : It means operations in node are drive or based 
+            by events         
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!-- Main Features and Advantage of node js -->
+
+    1. Asynchronous : Enables handling multiple concurrent requests & non blocking execution of thread.
+
+    2. V8 JS Engine : Built on Google's V8 JS engine, which compiles JS to machine code for fast execution.
+
+    
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+/**
   * @NEW_TOPIC <!--  -->
 
 
