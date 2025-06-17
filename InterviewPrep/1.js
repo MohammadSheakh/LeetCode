df48dbecad411d6f42f95c913c14f2c16a4e05eb
Mohammad Sheakh <mohammad.sheakh@gmail.com>
2025-06-17 19:19:49 +0600

    start interview prep

---
+++ InterviewPrep/1.js
@@ -0,0 +1,48 @@
+
+/**
+  * @NEW_TOPIC <!-- what is node js -->
+    node js is a runtime environment that allows us to run javascript on the server side
+
+    @Next <!--  runtime environment  -->
+    converting high level code to low level code
+    node js uses v8 engine to convert javascript code to machine code
+    v8 engine is developed by google and is used in chrome browser
+    node js is built on top of v8 engine
+ */
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+
+/**
+  * @NEW_TOPIC <!--  -->
+
+
+    @Next <!--    -->
+    
+ */
+
+
+/**
+  * @Steps <!--    -->
+  
+ */
+  
+
+
+
+
+  /**
+      * @Next <!--    -->
+      |
+      | ->> 
+      | 
+      +----------
+      | Ex: 
+     */
+  
